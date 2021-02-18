8568521824ef1c1010bdaaeaad326bbe64820024
tiduscrying <benhofb@gmail.com>
2021-02-18 02:29:32 -0500

    Additional launch script modifications
    Now should output to a log.txt file

--- release/Wolfenstein 3D - Spear of Destiny.sh
+++ release/Wolfenstein 3D - Spear of Destiny.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/ecwolf"
 export SDL_GAMECONTROLLERCONFIG="$(cat gamecontrollerdb.txt)"
-./ecwolf --config ./ecwolf.cfg --data sod
+./ecwolf --config ./ecwolf.cfg --data sod 2>&1 | tee -a ./log.txt
 unset SDL_GAMECONTROLLERCONFIG
